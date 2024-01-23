1cace2d85176f26c864202d8523a41f8c9175223
rikashi-shifu <harryliow229@gmail.com>
2024-01-23 16:26:36 +0800

    Request: Init

---
+++ packages/frontend/tailwind.config.ts
@@ -0,0 +1,13 @@
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+export default {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
