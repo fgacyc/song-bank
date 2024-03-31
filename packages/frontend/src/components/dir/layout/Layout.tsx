33a14d591f38251c538a87bcae3ed5fe0a8fe5dc
rikashi-shifu <harryliow229@gmail.com>
2024-03-31 17:39:20 +0800

    Feat: Added the appropriate title for each song in [song].tsx

--- packages/frontend/src/components/dir/layout/Layout.tsx
+++ packages/frontend/src/components/dir/layout/Layout.tsx
@@ -1,18 +1,26 @@
 import Head from "next/head";
 import React, { type ReactNode } from "react";
 import Header from "./Header";
+import { useRouter } from "next/router";
 
 interface DirLayoutProps {
   children: ReactNode;
 }
 
-const songName = "You Are Good";
-
 const Layout: React.FC<DirLayoutProps> = ({ children }) => {
+  const router = useRouter();
+
   return (
     <>
       <Head>
-        <title>{songName}</title>
+        <title>
+          {router.query.song &&
+            (router.query.song as string)
+              .replace(/-/g, " ")
+              .split(" ")
+              .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+              .join(" ")}
+        </title>
         <meta name="" content="" />
         <link rel="icon" href="/img/logo.png" />
       </Head>
