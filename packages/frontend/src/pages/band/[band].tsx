296337f78c72492ae7dd3f1e1fe19c3d7210d60d
rikashi-shifu <harryliow229@gmail.com>
2024-04-25 15:45:49 +0800

    Fix: Styling in band page

--- packages/frontend/src/pages/band/[band].tsx
+++ packages/frontend/src/pages/band/[band].tsx
@@ -207,7 +207,7 @@ const Band = () => {
           </div>
 
           {/* right */}
-          <div className="grid w-full gap-5 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+          <div className="grid h-fit w-full gap-5 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
             {uniqueAlbumList.map((album, i) => {
               const songsInAlbum = filteredSongListWithAlbum.filter(
                 (items) => items.album === album,
