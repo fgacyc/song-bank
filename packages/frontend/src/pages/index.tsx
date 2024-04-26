8e544ad25187fa16903424a0d8fb9d2d1655307a
rikashi-shifu <harryliow229@gmail.com>
2024-04-26 19:26:07 +0800

    Fix: Search bar animation not working in certain scenerio

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -2,17 +2,17 @@ import Footer from "@/components/index/Footer";
 import SearchBar from "@/components/index/SearchBar";
 import Head from "next/head";
 import Link from "next/link";
-import React, { useEffect, useRef, type RefObject } from "react";
+import React, { useLayoutEffect, useRef, type RefObject } from "react";
 import { IoPerson } from "react-icons/io5";
 
 const Home = () => {
   const inputRef: RefObject<HTMLInputElement> = useRef(null);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (inputRef.current) {
       inputRef.current.focus();
     }
-  }, []);
+  }, [inputRef]);
 
   return (
     <>
@@ -21,7 +21,7 @@ const Home = () => {
         <meta name="description" content="index" />
         <link rel="icon" href="/img/logo.png" />
       </Head>
-      <Link href={""} className="fixed right-0 p-5">
+      <Link href={"/"} className="fixed right-0 p-5">
         <IoPerson className="h-[20px] w-[20px]" />
       </Link>
       <SearchBar inputRef={inputRef} />
