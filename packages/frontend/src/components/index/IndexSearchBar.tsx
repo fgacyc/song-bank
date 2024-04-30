7b82778921f14220b7eb38ce6c65f0be558dbf5d
rikashi-shifu <harryliow229@gmail.com>
2024-04-30 23:58:14 +0800

    Fix: Remove unnecessary animation

--- packages/frontend/src/components/index/IndexSearchBar.tsx
+++ packages/frontend/src/components/index/IndexSearchBar.tsx
@@ -1,17 +1,11 @@
-import React, { type RefObject, useState } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { RiSearch2Line } from "react-icons/ri";
 import { useRouter } from "next/router";
 
-type IndexSearchBarProps = {
-  inputRef: RefObject<HTMLInputElement>;
-};
-
-const IndexSearchBar: React.FC<IndexSearchBarProps> = ({ inputRef }) => {
+const IndexSearchBar = () => {
   const [searchString, setSearchString] = useState("");
   const router = useRouter();
-
-  const [inputWidth, setInputWidth] = useState("max-w-[200px]");
 
   return (
     <div className="flex h-[50dvh] w-full flex-col items-center justify-end">
@@ -30,12 +24,9 @@ const IndexSearchBar: React.FC<IndexSearchBarProps> = ({ inputRef }) => {
           priority={true}
           className="pb-10"
         />
-        <div
-          className={`${inputWidth} flex w-[75%] items-center justify-center rounded-full border px-3 py-1 duration-500`}
-        >
+        <div className="flex w-[75%] max-w-[390px] items-center justify-center rounded-full border px-3 py-1 duration-500">
           <RiSearch2Line />
           <input
-            ref={inputRef}
             autoFocus
             placeholder="Search"
             className="w-full ps-2"
@@ -43,9 +34,6 @@ const IndexSearchBar: React.FC<IndexSearchBarProps> = ({ inputRef }) => {
             onChange={(e) => {
               setSearchString(e.target.value);
               localStorage.setItem("song-search", e.target.value.trim());
-            }}
-            onFocus={() => {
-              setInputWidth("max-w-[390px]");
             }}
           />
         </div>
--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -2,18 +2,9 @@ import IndexFooter from "@/components/index/IndexFooter";
 import IndexSearchBar from "@/components/index/IndexSearchBar";
 import Head from "next/head";
 import Link from "next/link";
-import React, { useLayoutEffect, useRef, type RefObject } from "react";
 import { IoPerson } from "react-icons/io5";
 
 const Home = () => {
-  const inputRef: RefObject<HTMLInputElement> = useRef(null);
-
-  useLayoutEffect(() => {
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, [inputRef]);
-
   return (
     <>
       <Head>
@@ -24,7 +15,7 @@ const Home = () => {
       <Link href={"/"} className="fixed right-0 p-5">
         <IoPerson className="h-[20px] w-[20px]" />
       </Link>
-      <IndexSearchBar inputRef={inputRef} />
+      <IndexSearchBar />
       <IndexFooter />
     </>
   );
