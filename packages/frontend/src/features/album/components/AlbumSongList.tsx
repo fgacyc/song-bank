1afd035961064df8b332b7a056789c94a826ecc3
rikashi-shifu <harryliow229@gmail.com>
2025-11-10 13:25:45 +0800

    feat: complete ui for album

--- packages/frontend/src/features/album/components/AlbumSongList.tsx
+++ packages/frontend/src/features/album/components/AlbumSongList.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import type { AlbumTypeWithSongs, SongType } from "../types/types";
+import Image from "next/image";
+import { CiImageOff } from "react-icons/ci";
+import { Badge } from "@/features/shared/ui/Badge";
+import Link from "next/link";
+import slugify from "slugify";
 
 interface AlbumSongProps {
   song: SongType;
@@ -8,17 +13,52 @@ interface AlbumSongProps {
 
 const AlbumSong = ({ song, index }: AlbumSongProps) => {
   return (
-    <div className="flex justify-between p-4">
-      <div className="flex border">
-        <div>{index + 1}</div>
-        <div>cover</div>
+    <Link
+      href={`/song/${song.id}/${slugify(song.name!, { lower: true })}`}
+      className="group flex justify-between p-4 group-hover:bg-bg-quaternary"
+    >
+      <div className="flex gap-7">
+        <div className="text-sm font-semibold text-text-secondary">
+          {index + 1}
+        </div>
+        <div className="relative h-12 w-20 overflow-hidden rounded-md">
+          {song.cover_image_url ? (
+            <Image
+              src={song.cover_image_url}
+              alt={song.name ?? "Untitled"}
+              fill
+              className="relative object-cover"
+            />
+          ) : (
+            <div className="flex h-60 w-60 items-center justify-center rounded-lg bg-bg-secondary text-text-secondary">
+              <CiImageOff />
+            </div>
+          )}
+        </div>
         <div>
-          <div>{song.name}</div>
-          <div>tag</div>
+          <div className="text-text-primary">{song.name}</div>
+          <div className="space-x-1">
+            <Badge variant="outline" className="rounded-md border-border">
+              {song.song_language}
+            </Badge>
+            {song.tags.map((tag) => {
+              return (
+                <Badge
+                  key={tag.id}
+                  variant="outline"
+                  className="rounded-md border-border"
+                >
+                  {tag.content}
+                </Badge>
+              );
+            })}
+          </div>
         </div>
       </div>
-      <div>key</div>
-    </div>
+      <div className="text-sm font-semibold text-text-secondary">
+        Key: {song.original_key}
+      </div>
+    </Link>
   );
 };
 
@@ -28,8 +68,8 @@ interface AlbumSongListProps {
 
 const AlbumSongList = ({ album }: AlbumSongListProps) => {
   return (
-    <div className="flex items-center justify-center px-4">
-      <div className="rounded-lg border border-border md:max-w-[70dvw]">
+    <div className="flex items-center justify-center px-4 pb-12">
+      <div className="w-full rounded-lg border border-border p-8 md:max-w-[60dvw]">
         {album.Song.map((song, index) => (
           <AlbumSong key={song.id} song={song} index={index} />
         ))}
